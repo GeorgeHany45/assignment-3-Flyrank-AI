c0682862dbc3e61281f9266305a478a86945ed68
George Hany <georgehany562002@gmail.com>
2026-08-10 16:42:37 +0300

    created the login and signup

--- src/server.js
+++ src/server.js
@@ -18,6 +18,57 @@ app.get("/", (req, res) => {
   res.json({ message: "Auth API is running" });
 });
 
+app.post("/auth/signup", async (req, res) => {
+  const { email, password } = req.body;
+
+  
+  if (!email || !password) {
+    return res.status(400).json({
+      error: "Email and password are required",
+    });
+  }
+
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+
+  if (error) {
+    return res.status(400).json({
+      error: error.message,
+    });
+  }
+
+  return res.status(201).json({
+    user: data.user,
+  });
+});
+app.post("/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      error: "Email and password are required",
+    });
+  }
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    return res.status(401).json({
+      error: "Invalid login credentials",
+    });
+  }
+
+  return res.status(200).json({
+    access_token: data.session.access_token,
+    refresh_token: data.session.refresh_token,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log("Server running and connected to Supabase");
